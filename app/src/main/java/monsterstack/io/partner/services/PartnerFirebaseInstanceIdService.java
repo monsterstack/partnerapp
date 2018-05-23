723ffad250b7ed2ad40b8f22112c964a62e2c4b6
Zachary Rote <zacharyrote@Zacharys-MacBook-Pro.local>
2018-05-22 21:52:34 -0400

    add cloud messaging integration

---
+++ app/src/main/java/monsterstack/io/partner/services/PartnerFirebaseInstanceIdService.java
@@ -0,0 +1,52 @@
+package monsterstack.io.partner.services;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import monsterstack.io.api.ServiceLocator;
+import monsterstack.io.api.UserSessionManager;
+import monsterstack.io.api.custom.UserServiceCustom;
+import monsterstack.io.api.listeners.OnResponseListener;
+import monsterstack.io.api.resources.AuthenticatedUser;
+import monsterstack.io.api.resources.HttpError;
+import monsterstack.io.api.resources.User;
+
+public class PartnerFirebaseInstanceIdService extends FirebaseInstanceIdService {
+    private static final String TAG = "FCM";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+
+    private void sendRegistrationToServer(String refreshToken) {
+        Context context = getApplicationContext();
+
+        final UserSessionManager userSessionManager = new UserSessionManager(context);
+        AuthenticatedUser authenticatedUser = userSessionManager.getUserDetails();
+
+        if (null != authenticatedUser.getId()) {
+            // Set device id
+            //authenticatedUser.setDeviceId(refreshToken);
+
+            UserServiceCustom userServiceCustom = ServiceLocator.getInstance(context).getUserService();
+            userServiceCustom.updateUser(authenticatedUser.getId(), authenticatedUser, new OnResponseListener<User, HttpError>() {
+                @Override
+                public void onResponse(User user, HttpError httpError) {
+                    userSessionManager.createUserSession(new AuthenticatedUser(user));
+                }
+            });
+        }
+    }
+}
