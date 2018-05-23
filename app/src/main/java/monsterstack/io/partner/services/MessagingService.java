b615d6dfd7db6bbf522a70013ecc138c0043e2ae
Zachary Rote <zacharyrote@Zacharys-MacBook-Pro.local>
2018-05-23 19:47:17 -0400

    subscribe to topic

--- app/src/main/java/monsterstack/io/partner/services/MessagingService.java
+++ app/src/main/java/monsterstack/io/partner/services/MessagingService.java
@@ -1,9 +1,11 @@
 package monsterstack.io.partner.services;
 
 import android.content.Context;
+import android.support.annotation.WorkerThread;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -14,6 +16,8 @@ public class MessagingService extends FirebaseMessagingService {
     public static void initialize(Context context) {
         String token = FirebaseInstanceId.getInstance().getToken();
         new PartnerFirebaseInstanceIdService().sendRegistrationToServer(token, context);
+
+        FirebaseMessaging.getInstance().subscribeToTopic(PARTNER_TOPIC);
     }
 
     @Override
@@ -45,6 +49,12 @@ public class MessagingService extends FirebaseMessagingService {
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+    @Override
+    @WorkerThread
+    public void onMessageSent(String s) {
+        Log.d(TAG, String.format("Sent message %s", s));
     }
 
     private void scheduleJob() {
